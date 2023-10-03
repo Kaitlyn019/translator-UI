10b62b7ca0d40829b8e468dd241f4cc73d1e6b45
Kaitlyn019 <kaitlynyyang@gmail.com>
2023-10-03 17:45:18 -0400

    Revert "client and server side up"
    This reverts commit 533d542e8bac7fcc4b471d425d98d502ec0a8192.

--- translator/views.py
+++ translator/views.py
@@ -4,11 +4,6 @@ def index(request):
     return render(request, 'index.html')
 
 def room(request, room_name):
-    if room_name == 'client':
-        return render(request, 'client.html', {
-            'room_name': 'test'
-        })
-    else:
-        return render(request, 'server.html', {
-            'room_name': 'test'
-        })
+    return render(request, 'room.html', {
+        'room_name': room_name
+    })
