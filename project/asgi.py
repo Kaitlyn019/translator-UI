c79add3f5b7f833de3c7d069bc94c56a7096df07
Kaitlyn019 <kaitlynyyang@gmail.com>
2023-10-03 17:31:13 -0400

    last attempt

--- project/asgi.py
+++ project/asgi.py
@@ -8,7 +8,7 @@ import translator.routing
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "https": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
             translator.routing.websocket_urlpatterns
