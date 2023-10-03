3d97bc68fdfc36140aad96ffc09e10526961362d
Kaitlyn019 <kaitlynyyang@gmail.com>
2023-10-03 13:29:03 -0400

    Trying to rollback

--- project/asgi.py
+++ project/asgi.py
@@ -2,28 +2,16 @@ import os
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from django.urls import path, re_path
+import translator.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
-
-from translator.consumers import ChatConsumer
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = ProtocolTypeRouter({
-    # Django's ASGI application to handle traditional HTTP requests
-    "http": django_asgi_app,
-
-    # WebSocket chat handler
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter([
-                path("chat/admin/", ChatConsumer.as_asgi()),
-                re_path(r'ws/chat/(?P<room_name>\w+)/$', ChatConsumer.as_asgi()),
-            ])
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            translator.routing.websocket_urlpatterns
         )
     ),
 })
--- translator/routing.py
+++ translator/routing.py
@@ -2,12 +2,6 @@ from django.urls import re_path
 
 from . import consumers
 
-channel_routing = {
-    'websocket.connect': consumers.ChatConsumer.connect,
-    'websocket.receive': consumers.ChatConsumer.receive,
-    'websocket.disconnect': consumers.ChatConsumer.disconnect,
-}
-
 websocket_urlpatterns = [
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
